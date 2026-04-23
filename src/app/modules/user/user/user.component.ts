f5b84578ac2743d02059b61ec3a770a1c6c3f3b9
paulelyson <paulelyson.villaceran@gmail.com>
2026-04-24 00:10:32 +0800

    feat: add user module with routing and component
    - Updated angular.json to include schematics for various Angular components and services.
    - Modified app.html to remove placeholder content and retain only the router outlet.
    - Defined routes in app.routes.ts to lazy load the user module.
    - Created user module with UserComponent and UserRoutingModule.
    - Added user.component.html and user.component.css for the UserComponent.
    - Updated tsconfig.app.json to set rootDir to ./src.

---
+++ src/app/modules/user/user/user.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css',
+  standalone: false
+})
+export class UserComponent {}
