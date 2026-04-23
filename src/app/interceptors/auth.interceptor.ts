d6dbccaad0d8642d634c2f4f00705bccc28b368e
paulelyson <paulelyson.villaceran@gmail.com>
2026-04-24 00:58:32 +0800

    feat: add auth interceptor for handling authorization tokens

---
+++ src/app/interceptors/auth.interceptor.ts
@@ -0,0 +1,13 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+
+export const authInterceptor: HttpInterceptorFn = (req, next) => {
+  const token = localStorage.getItem('token');
+
+  if (token) {
+    req = req.clone({ setHeaders: { Authorization: 'Bearer ' + token } });
+  } else {
+    // TODO: logout
+  }
+  return next(req);
+};
