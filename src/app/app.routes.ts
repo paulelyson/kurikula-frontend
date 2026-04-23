dead935068f5bcb4f7483ca857762edda4f6fbc7
paulelyson <paulelyson.villaceran@gmail.com>
2026-04-24 00:16:19 +0800

    feat: add course module with routing and component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,14 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
- {
-  path: 'user',
-  loadChildren: () => import('./modules/user/user-routing-module').then(m => m.UserRoutingModule),
- }
+  {
+    path: 'user',
+    loadChildren: () =>
+      import('./modules/user/user-routing-module').then((m) => m.UserRoutingModule),
+  },
+  {
+    path: 'course',
+    loadChildren: () =>
+      import('./modules/course/course-routing.module').then((m) => m.CourseRoutingModule),
+  },
 ];
---
+++ src/app/modules/course/course/course.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-course',
+  templateUrl: './course.component.html',
+  styleUrl: './course.component.css',
+  standalone: false
+})
+export class CourseComponent {}
