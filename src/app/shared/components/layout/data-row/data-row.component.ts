6ace7769473453cb4dc83919bed6945eaceb42fe
paulelyson <paulelyson.villaceran@gmail.com>
2026-04-24 18:55:47 +0800

    feat: add data-row component with HTML, CSS, and TypeScript files

---
+++ src/app/shared/components/layout/data-row/data-row.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-data-row',
+  imports: [],
+  templateUrl: './data-row.component.html',
+  styleUrl: './data-row.component.css',
+})
+export class DataRowComponent {}
