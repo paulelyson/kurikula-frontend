2e9e6edeb18bc578aa09a489572793104173afcc
paulelyson <paulelyson.villaceran@gmail.com>
2026-04-25 10:50:31 +0800

    feat: add TitleComponent and integrate it into UserModule and UserComponent

--- src/app/modules/user/user-module.ts
+++ src/app/modules/user/user-module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { UserRoutingModule } from './user-routing-module';
 import { UserComponent } from './user/user.component';
 import { DataRowComponent } from '../../shared/components/layout/data-row/data-row.component';
+import { TitleComponent } from '../../shared/components/layout/title/title.component';
 
 @NgModule({
   declarations: [UserComponent],
-  imports: [CommonModule, UserRoutingModule, DataRowComponent],
+  imports: [CommonModule, UserRoutingModule, DataRowComponent, TitleComponent],
 })
 export class UserModule {}
