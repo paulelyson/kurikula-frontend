644bb8a02dade70a68aa93fce6fdde736f49396e
paulelyson <paulelyson.villaceran@gmail.com>
2026-04-25 16:17:16 +0800

    feat: enhance InputComponent with additional properties and implement ControlValueAccessor for better form integration

--- src/app/shared/components/forms/input/input.component.ts
+++ src/app/shared/components/forms/input/input.component.ts
@@ -1,9 +1,61 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, forwardRef, Input, Output } from '@angular/core';
+import { ControlValueAccessor, FormsModule, NG_VALUE_ACCESSOR } from '@angular/forms';
+import { FloatLabelType, MatFormFieldAppearance, MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+
+type InputSize = 'sm' | 'md' | 'lg';
 
 @Component({
   selector: 'app-input',
-  imports: [],
+  imports: [CommonModule, MatFormFieldModule, MatInputModule, FormsModule, MatIconModule],
   templateUrl: './input.component.html',
   styleUrl: './input.component.css',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => InputComponent),
+      multi: true,
+    },
+  ],
 })
-export class InputComponent {}
+export class InputComponent implements ControlValueAccessor {
+  @Input() label: string = '';
+  @Input() placeholder: string = '';
+  @Input() suffix_icon: string = '';
+  @Input() suffix_icon_clickable: boolean = false;
+  @Input() appearance: MatFormFieldAppearance = 'fill';
+  @Input() floatLabel: FloatLabelType = 'always';
+  @Input() inputType: string = 'text';
+  @Input() width: InputSize = 'md';
+  // accessor
+  @Input() value: string = '';
+  @Input() disabled: boolean = false;
+  @Input() readonly: boolean = false;
+  @Output() suffixIconClick = new EventEmitter<void>();
+
+  public changed = (_: any) => {};
+  public touched = () => {};
+
+  writeValue(value: any): void {
+    this.value = value;
+  }
+  registerOnChange(fn: any): void {
+    this.changed = fn;
+  }
+  registerOnTouched(fn: any): void {
+    this.touched = fn;
+  }
+  setDisabledState?(isDisabled: boolean): void {
+    this.disabled = isDisabled;
+  }
+
+  onInput(event: Event) {
+    this.changed(this.value);
+  }
+
+  onSuffixIconClick() {
+    this.suffixIconClick.emit();
+  }
+}
