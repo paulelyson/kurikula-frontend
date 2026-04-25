d2c54297e2ef74939b816b227dfd6e303d93ee50
paulelyson <paulelyson.villaceran@gmail.com>
2026-04-25 17:25:12 +0800

    feat: implement registration form layout and styles with additional input fields

--- src/app/modules/auth/register/register.component.ts
+++ src/app/modules/auth/register/register.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { InputComponent } from '../../../shared/components/forms/input/input.component';
+import { TitleComponent } from '../../../shared/components/layout/title/title.component';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [InputComponent, TitleComponent],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css',
 })
