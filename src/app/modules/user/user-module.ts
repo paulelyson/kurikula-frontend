def1bd5b994a18589d5b15d90dcd0a636d0d1812
paulelyson <paulelyson.villaceran@gmail.com>
2026-04-25 11:52:41 +0800

    feat: add UserToolbarComponent and integrate it into UserModule and UserComponent

--- src/app/modules/user/user-module.ts
+++ src/app/modules/user/user-module.ts
@@ -5,9 +5,10 @@ import { UserRoutingModule } from './user-routing-module';
 import { UserComponent } from './user/user.component';
 import { DataRowComponent } from '../../shared/components/layout/data-row/data-row.component';
 import { TitleComponent } from '../../shared/components/layout/title/title.component';
+import { UserToolbarComponent } from './user-toolbar/user-toolbar.component';
 
 @NgModule({
-  declarations: [UserComponent],
+  declarations: [UserComponent, UserToolbarComponent],
   imports: [CommonModule, UserRoutingModule, DataRowComponent, TitleComponent],
 })
 export class UserModule {}
---
+++ src/app/modules/user/user-toolbar/user-toolbar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-toolbar',
+  templateUrl: './user-toolbar.component.html',
+  styleUrl: './user-toolbar.component.css',
+  standalone: false,
+})
+export class UserToolbarComponent {
+  
+}
