78927fd6e3fb32d81c804b9177fa26da9ede0a30
paulelyson <paulelyson.villaceran@gmail.com>
2026-04-25 15:50:50 +0800

    feat: create InputComponent and integrate it into UserToolbarComponent with updated styles

--- src/app/modules/user/user-toolbar/user-toolbar.component.ts
+++ src/app/modules/user/user-toolbar/user-toolbar.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { InputComponent } from '../../../shared/components/forms/input/input.component';
 
 @Component({
   selector: 'app-user-toolbar',
   templateUrl: './user-toolbar.component.html',
   styleUrl: './user-toolbar.component.css',
+  imports: [InputComponent]
 })
 export class UserToolbarComponent {
   
