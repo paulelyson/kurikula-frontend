dda3759fb77a74d340fbac731b480bcf6a1df550
paulelyson <paulelyson.villaceran@gmail.com>
2026-04-25 17:29:22 +0800

    feat: add ButtonComponent and integrate it into the registration form

--- src/app/modules/auth/register/register.component.ts
+++ src/app/modules/auth/register/register.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { InputComponent } from '../../../shared/components/forms/input/input.component';
 import { TitleComponent } from '../../../shared/components/layout/title/title.component';
+import { ButtonComponent } from '../../../shared/components/ui/button/button.component';
 
 @Component({
   selector: 'app-register',
-  imports: [InputComponent, TitleComponent],
+  imports: [InputComponent, TitleComponent, ButtonComponent],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css',
 })
