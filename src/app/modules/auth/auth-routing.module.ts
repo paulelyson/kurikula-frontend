d093f80067e497189fec92748c872e2254bd2a76
paulelyson <paulelyson.villaceran@gmail.com>
2026-04-28 13:53:34 +0800

    feat: add Home component with template and styles; update routing to include Home path

--- src/app/modules/auth/auth-routing.module.ts
+++ src/app/modules/auth/auth-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
+   {
+    path: '',
+    component: HomeComponent
+  },
   {
     path: 'register',
     component: RegisterComponent
