3b707bf17e7280a07f8e04e7284de4e308e0ae51
paulelyson <paulelyson.villaceran@gmail.com>
2026-04-28 13:09:05 +0800

    feat: implement Avatar component with styling and structure; update header background color variables

--- src/app/shared/components/ui/avatar/avatar.component.ts
+++ src/app/shared/components/ui/avatar/avatar.component.ts
@@ -1,9 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Size } from '../../../../models/ui/common.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-avatar',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './avatar.component.html',
   styleUrl: './avatar.component.css',
 })
-export class AvatarComponent {}
+export class AvatarComponent {
+  @Input() size: Size = 'md';
+  @Input() label: string = ''
+  @Output() avatarclicked: EventEmitter<string> = new EventEmitter();
+  image = 'https://placehold.co/60?text=No+Image&font=poppins';
+
+}
